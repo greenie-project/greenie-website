a8905e708a79a0ce8a89344827f7dbbdb97c2a63
Lohhan <lohhang05@gmail.com>
2024-05-27 21:21:28 -0300

    Homepage Start

---
+++ scripts-js/script.js
@@ -0,0 +1,12 @@
+// Nome flutuante dos ícones da navbar
+const iconNames = document.querySelectorAll(".icon-name");
+
+iconNames.forEach((iconName) => {
+  iconName.parentNode.addEventListener("mousemove", (event) => {
+    const newX = event.clientX - iconName.offsetWidth - 10;
+    const newY = event.clientY - iconName.offsetHeight / 2;
+
+    iconName.style.left = `${newX}px`;
+    iconName.style.top = `${newY}px`;
+  });
+});
